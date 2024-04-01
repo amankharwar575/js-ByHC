f0f931a89efd0c3a11b4ce7a31e22a157d9c765f
amankharwar575 <91006649+amankharwar575@users.noreply.github.com>
2024-04-01 12:00:05 +0000

    conversion

---
+++ basic/03_conversion.js
@@ -0,0 +1,18 @@
+let score =true
+
+let valueInNumber = Number(score)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// "33"=>33
+// "33abs"=>NAN
+// true => 1; false=>0
+let isLoggedIn=1
+let booleanLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanLoggedIn);
+// 1=>true; 0=>false
+// ""=> false
+// "hitesh"=> true
+
+let someNumber=56
+let stringNumber = String(someNumber)
+console.log( typeof stringNumber)

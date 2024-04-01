7a531af6e14299868c9bac8f7bbaa29f57c7c14f
amankharwar575 <91006649+amankharwar575@users.noreply.github.com>
2024-04-01 11:37:51 +0000

    02_datatype

---
+++ basic/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict" ;
+/* 
+treat all js code as newer version 
+alert (3+3) we are using not brower
+*/
+// code readablibilty is mu
+console.log(3+3);
+console.log("Aman");
+let name = "kharwar"
+let age =23
+let isLoggedIn = false
+let state = null
+// number=> to power 53
+// bigint
+// string ""
+// boolean =>true/false
+// null standalone value
+// undefined=>
+// symbol =>unique
+// objects
+console.log(typeof "hitesh");
+console.log(typeof null);
+console.log(typeof undefined)
+
+
+

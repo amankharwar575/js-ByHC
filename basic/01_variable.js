364b7d725216a75bda7396ac48fd0f9196ff30eb
amankharwar575 <91006649+amankharwar575@users.noreply.github.com>
2024-04-01 11:19:49 +0000

    controler code commit

---
+++ basic/01_variable.js
@@ -0,0 +1,21 @@
+const accountId=5689
+let accountEmail="amankharwar575@gmail"
+var accountPassword="2356"
+accountCity="Mumbai"
+let accountState;
+// const cant be change 
+// accountId=2 it is not allowed 
+// {
+    // it is called scope 
+    // in instial there was problem i scope
+
+// }
+/*
+prefer not to use var 
+*/
+accountEmail="ak@hk.com"
+accountPassword="78934"
+accountCity="chennai"
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity,accountState])
+
